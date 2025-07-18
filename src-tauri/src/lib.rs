98b9f3da9daf814ee516ae840db92b7e41a047b0
akorzunin <akorzunin123@gmail.com>
2025-07-19 00:53:24 +0300

    feat: added tauri fs plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .setup(|app| {
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ForceGraph } from "./components/ForceGraph";
+import { MenuBar } from "./components/MenuBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <MenuBar />
     <ForceGraph />
   </React.StrictMode>
 );
